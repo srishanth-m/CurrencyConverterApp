bd72bdf56aca61f4f5c69149deaf0ad31f8ea1e8
srihanth-m <srishanthmule@gmail.com>
2023-10-09 13:08:10 +0530

    currency converter site ready

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
-const url = "https://api.currencyapi.com/v3/latest?apikey=cur_live_gIccEybwSX0VWXphf7LDOxMrbDwarOKxG8Mxauyr"
+
+const populate = async (value , currency) =>{
+    let mystr = ""
+    const url = "https://api.currencyapi.com/v3/latest?apikey=cur_live_gIccEybwSX0VWXphf7LDOxMrbDwarOKxG8Mxauyr&base_currency" + currency
+    let response = await fetch(url)
+    let rjson = await response.json();
+    document.querySelector(".output").style.display = "block";
+
+    for(let key of Object.keys(rjson["data"])){
+        mystr += `
+            <tr>
+                <td>${key}</td>
+                <td>${rjson["data"][key]["code"]}</td>
+                <td>${Math.round(rjson["data"][key]["value"] * value)}</td>
+            </tr>
+            `
+    }
+    const tableBody = document.querySelector('tbody');
+    tableBody.innerHTML = mystr;
+}
+const btn = document.querySelector('.btn');
+btn.addEventListener('click' , (e) =>{
+    e.preventDefault();
+    const value = parseInt(document.querySelector("input[name='quantity']").value);
+    const currency = document.querySelector("select[name='currency']").value;
+    populate(value ,currency);
+})
